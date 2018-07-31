6c405814ae6b921a3bc6b9ba2c807c079d18facc
Andrew Staffell <andrew@yeswework.com>
2018-07-31 18:22:28 +0200

    add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Fabrica Evergreen Web
+FEW is a WordPress plugin developed by [Yes We Work](http://yeswework.com/) that implements the 4042302 'evergreen web' technique developed by by [Aral Balkan](https://ar.al/).
+
+## Installation
+Download the zip file and install in WordPress via Plugins > Add New > Upload Plugin, then activate the plugin.
+
+Enter the fallback path at Settings > Fabrica Evergreen Web.
--- fabrica-evergreen-web.php
+++ fabrica-evergreen-web.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Fabrica Evergreen Web
 Plugin URI: https://fabri.ca/
-Description: WordPress implementation of the <a href="https://4042302.org/">4042302</a> 'evergreen web' technique developed by <a href="https://ar.al.org/">Aral Balkan</a>.
-Version: 0.1
+Description: WordPress implementation of the <a href="https://4042302.org/">4042302</a> 'evergreen web' technique developed by <a href="https://ar.al/">Aral Balkan</a>.
+Version: 0.1.0
 Author: Yes we work
 Author URI: https://yeswework.com/
 Text Domain: fabrica-evergreen-web
